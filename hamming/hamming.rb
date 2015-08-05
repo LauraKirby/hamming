780b3ba30134c36e05de74b0de393acc33577a34
LauraKirby <laurakirby26@gmail.com>
2015-08-04 17:53:39 -0700

    exercism

---
+++ hamming/hamming.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Hamming
+
+	attr_accessor :counter
+
+	def initialize
+		@counter = 0
+	end
+
+	def compute(dna_1, dna_2)
+		dna_1_array = dna_1.split('')
+		dna_2_array = dna_2.split('')
+
+		dna_length = dna_1_array.length
+
+    for dna_1_array in dna_1_array.length
+
+    end
+
+
+
+    @counter 
+	end
+
+end
+
+
+blah = Hamming.new
+
+blah.compute("ADLK","CDDD")
